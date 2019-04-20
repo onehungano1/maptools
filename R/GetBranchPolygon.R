2e6356d4c63f9b560e8989d2227d48ed672e33fa
frank <frank@parrotanalytics.com>
2019-04-20 23:29:22 +1200

    first commit

--- R/GetBranchPolygon.R
+++ R/GetBranchPolygon.R
@@ -1,12 +1,15 @@
-
-
-# read in input csv file
-branches <- read.csv("asbmaptest.csv")
-branches <- read.csv("banklocations.csv", encoding="UTF-8") 
-colnames(branches) <- c(colnames(branches)[1:2], c("address", 'lat', 'lng'), colnames(branches)[6:9])
-head(branches)
-
 # define function for calling the OSRM API
+#' Title get polygon using OSRM given defalt 0 - 30 increamental 5 mins drive time
+#'
+#' @param x dataframe contain some info
+#'
+#' @return spatial dataframe containing the result
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' getPolygonsfromOSRM(ds)
+#' }
 getPolygonsfromOSRM <- function(x){
   branch <- data.frame(x)
   branch.spdf <-  osrmIsochrone(loc = c(branch$lng, branch$lat), breaks = seq(from = 0,to = 30, by = 5))
@@ -18,27 +21,6 @@ getPolygonsfromOSRM <- function(x){
   branch.spdf@data$AU_Name <- branch$AU_Name
   branch.spdf@data$Region <- branch$Region
   branch.spdf@data$Super_Region <- branch$Super_Region
-  return(branch.spdf) 
+  return(branch.spdf)
 }
-
-
-branches.spdf <- list(nrow(branches))
-for (i in c(1:nrow(branches.spdf))){
-  branches.spdf[i] <- getPolygonsfromOSRM(branches[i,])
-}
-
-
-
-
-names(branches.spdf)(paste(branches$Bank, '--', branches$address, sep=''))
-
-
-#save data as rds
-filename <- paste("branches_polygons.rds_", Sys.Date(), sep = '')
-saveRDS(branches.spdf, file = filename)
-#819 is NULL
-
-#restore data in different name and validate if two data are identical
-branch.spdfs <- readRDS(filename)
-assert_that(identical(branches.spdf, branch.spdfs))
 
